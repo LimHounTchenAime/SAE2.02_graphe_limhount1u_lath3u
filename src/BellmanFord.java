2900b4e960915f43ce0ed20666cf1a48d5d17fb5
lath3u <victor.lath9@etu.univ-lorraine.fr>
2022-06-13 14:05:57 +0200

    ajout methode resoudre

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -3,12 +3,26 @@ public class BellmanFord {
 
     public Valeur resoudre(Graphe g, String depart) {
         Valeur v = new Valeur();
-        //initiatlisation valeur
+        //initialisation
         for (int i = 0; i < g.listeNoeuds().size(); i++) {
-            if (g.listeNoeuds().get(i) == depart) {
-                v.setValeur(depart, 0);
+            v.setValeur(g.listeNoeuds().get(i), Double.MAX_VALUE);
+        }
+        v.setValeur(depart, 0);
+
+        for (int i = 0; i < g.listeNoeuds().size(); i++) {
+            //successeur du noeuds
+            for (int k = 0; k < g.suivants(g.listeNoeuds().get(i)).size(); k++) {
+                String u = g.listeNoeuds().get(i);
+                String parent = v.getParent(u);
+                double arc_uv = g.suivants(g.listeNoeuds().get(i)).get(k).getCout();
+                double tmp = v.getValeur(u) + arc_uv;
+                if (tmp < v.getValeur(parent)) {
+                    v.setValeur(parent, tmp);
+                    v.setParent(parent, u);
+                }
             }
         }
+
         return v;
     }
 }
