6cee9eb62963610704192105861cca029322e8da
lath3u <victor.lath9@etu.univ-lorraine.fr>
2022-06-13 11:08:25 +0200

    test

--- Test/GrapheListeTest.java
+++ Test/GrapheListeTest.java
@@ -1,29 +1,73 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GrapheListeTest {
 
     @org.junit.jupiter.api.Test
-    void ajouterArc() {
+    void TestAjouterArc() {
         GrapheListe gl = new GrapheListe();
 
-        assertEquals(0,gl.listeNoeuds().size());
+        assertEquals(0, gl.listeNoeuds().size());
 
         //premier ajout
-        gl.ajouterArc("A","B",120);
-        assertEquals(2,gl.listeNoeuds().size());
-        assertEquals("A",gl.listeNoeuds().get(0));
+        gl.ajouterArc("A", "B", 120);
+        assertEquals(2, gl.listeNoeuds().size());
+        assertEquals("A", gl.listeNoeuds().get(0));
 
         //deuxieme ajout avec le meme depart
-        gl.ajouterArc("A","D",87);
-        assertEquals("A",gl.listeNoeuds().get(0));
-        assertEquals(3,gl.listeNoeuds().size());
+        gl.ajouterArc("A", "D", 87);
+        assertEquals("A", gl.listeNoeuds().get(0));
+        assertEquals(3, gl.listeNoeuds().size());
 
         //
-        gl.ajouterArc("D","C",10);
-        assertEquals(4,gl.listeNoeuds().size());
+        gl.ajouterArc("D", "C", 10);
+        assertEquals(4, gl.listeNoeuds().size());
 
 
     }
 
-    //TODO TEST METHODES
+    @Test
+    void TesttoGraphViz() {
+        GrapheListe gl = new GrapheListe();
+        gl.ajouterArc("A", "B", 12);
+        gl.ajouterArc("A", "D", 87);
+        gl.ajouterArc("B", "E", 11);
+        gl.ajouterArc("C", "A", 19);
+        gl.ajouterArc("D", "C", 10);
+        gl.ajouterArc("D", "B", 23);
+        gl.ajouterArc("E", "D", 43);
+        // la méthode ne toGraphViz ne trie pas les noeuds par ordre alphabétique
+        String res = "digraph {\n" +
+                "A -> B [label = 12]\n" +
+                "A -> D [label = 87]\n" +
+                "B -> E [label = 11]\n" +
+                "D -> C [label = 10]\n" +
+                "D -> B [label = 23]\n" +
+                "E -> D [label = 43]\n" +
+                "C -> A [label = 19]\n" +
+                "}";
+
+        assertTrue(res.equals(gl.toGraphviz()));
+    }
+    @Test
+    void TesttoString(){
+        GrapheListe gl = new GrapheListe();
+        gl.ajouterArc("A", "B", 12);
+        gl.ajouterArc("A", "D", 87);
+        gl.ajouterArc("B", "E", 11);
+        gl.ajouterArc("C", "A", 19);
+        gl.ajouterArc("D", "C", 10);
+        gl.ajouterArc("D", "B", 23);
+        gl.ajouterArc("E", "D", 43);
+        String res = "A -> B(12) D(87) \n" +
+                "B -> E(11) \n" +
+                "D -> C(10) B(23) \n" +
+                "E -> D(43) \n" +
+                "C -> A(19) \n";
+
+        assertEquals(res, gl.toString());
+    }
+
 }
--- src/main.java
+++ src/main.java
@@ -1,10 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        Noeud a = new Noeud("A");
-        Noeud b = new Noeud("B");
-        Noeud c = new Noeud("C");
-        Noeud d = new Noeud("D");
-        Noeud e = new Noeud("E");
+
 
         GrapheListe gl = new GrapheListe();
         gl.ajouterArc("A", "B", 12);
