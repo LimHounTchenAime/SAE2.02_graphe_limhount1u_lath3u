63dab3c9327f26c720caedaa286d2c6b4a616000
lath3u <victor.lath9@etu.univ-lorraine.fr>
2022-06-14 10:09:04 +0200

    maj commentaire

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -12,6 +12,7 @@ public class BellmanFord {
             v.setParent(g.listeNoeuds().get(i), null);
         }
         v.setValeur(depart, 0);
+
         //début
         List<String> sommets = g.listeNoeuds();
         String vtmp = null;
@@ -22,23 +23,22 @@ public class BellmanFord {
             //pour chaque sommet
             for (int i = 0; i < sommets.size(); i++) {
                 String sommet_u = g.listeNoeuds().get(i);
-                //pour chaque arc du sommet u
+                //pour chaque arc du sommet_u
                 for (int k = 0; k < g.suivants(sommet_u).size(); k++) {
-                    //sommet_v est un des parents de sommet_u
+                    //sommet_v est un des sucesseurs du sommet_u
                     String sommet_v = g.suivants(sommet_u).get(k).getDest();
                     //on prend la valeur d'un des arcs partant de u
                     double cout_arc_uv = g.suivants(sommet_u).get(k).getCout();
                     //on prend la valeur de sommet_u(antécédent de sommet_v) + le coût de l'arc(u,v)
                     double tmp = v.getValeur(sommet_u) + cout_arc_uv;
-                    //si la nouvelle distance est meilleur que la valeur de sommet_v
+                    //si la nouvelle distance est meilleur que la valeur du sommet_v
                     if (tmp < v.getValeur(sommet_v)) {
+                        //on met la nouvelle valeur du sommet_v
                         v.setValeur(sommet_v, tmp);
                         //on met en parent le sommet_u
                         v.setParent(sommet_v, sommet_u);
                     }
-
                 }
-
             }
             cpt++;
         }
