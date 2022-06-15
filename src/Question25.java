e640c9c5e861efe4afddfd7963bad82b8831454e
Aimé <aime.lim-houn-tchen5@etu.univ-lorraine.fr>
2022-06-15 22:09:01 +0200

    Question 25

--- src/Question25.java
+++ src/Question25.java
@@ -1,7 +1,11 @@
-public class test {
+public class Question25 {
     public static void main(String[] args) {
-        GrapheListe grapheListe=GrapheListe.genererGraphe(10);
+        //genere des graphes de taille 10
+        GrapheListe grapheListe= GrapheListe.genererGraphe(10);
+
         System.out.println(grapheListe.toGraphviz());
+
+        //cherche le chemin minimum avec les deux algorithmes
         Algorithme algorithme=new BellmanFord();
         Valeur v=algorithme.resoudre(grapheListe, "n1");
         System.out.println(v);
