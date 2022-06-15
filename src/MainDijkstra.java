0cd6549e877cdf7173a3c3aea38e7f568ffb8f58
victorL <victor.lath9@etu.univ-lorraine.fr>
2022-06-15 23:03:29 +0200

    reorganisation

--- src/MainDijkstra.java
+++ src/MainDijkstra.java
@@ -1,7 +1,7 @@
 public class MainDijkstra {
     public static void main(String[] args) {
         Dijkstra dijkstra=new Dijkstra();
-        GrapheListe grapheListe=new GrapheListe("graphes/test1.txt");
+        GrapheListe grapheListe=new GrapheListe("graphes/graphe_exemple1.txt");
         Valeur cheminMinimum=dijkstra.resoudre(grapheListe, "A");
         System.out.println(cheminMinimum.calculerChemin("C"));
         System.out.println(cheminMinimum.calculerChemin("D"));
--- src/Question25.java
+++ src/Question25.java
@@ -1,19 +1,32 @@
 public class Question25 {
     public static void main(String[] args) {
         //genere des graphes de taille 10
-        GrapheListe grapheListe= GrapheListe.genererGraphe(10);
-        //Génére le graphe au format graphviz
-        System.out.println(grapheListe.toGraphviz());
+        GrapheListe grapheListe = GrapheListe.genererGraphe(3000);
 
         //cherche le chemin minimum avec les deux algorithmes
-        Algorithme algorithme=new BellmanFord();
-        Valeur v=algorithme.resoudre(grapheListe, "n1");
-        System.out.println(v);
-        System.out.println(v.calculerChemin("n9"));
+        //On calcule la vitesse de Bellmandford
+        Algorithme algorithme = new BellmanFord();
+        long startTime = System.nanoTime();
+        Valeur v = algorithme.resoudre(grapheListe, "n1");
+        long endTime = System.nanoTime();
+        long temps_BellmanFord = (endTime - startTime);
+        System.out.println("BellmanFord : " + temps_BellmanFord);
 
-        algorithme=new Dijkstra();
-        v=algorithme.resoudre(grapheListe, "n1");
-        System.out.println(v);
-        System.out.println(v.calculerChemin("n9"));
+
+        //On calcule la vitesse de Dijkstra
+        algorithme = new Dijkstra();
+        startTime = System.nanoTime();
+        v = algorithme.resoudre(grapheListe, "n1");
+        endTime = System.nanoTime();
+        long temps_Dijkstra= (endTime - startTime);
+        System.out.println("Dijkstra : " +  temps_Dijkstra);
+
+        String fast;
+        if(temps_Dijkstra>temps_BellmanFord){
+            fast = "BellmanFord";
+        }else {
+            fast ="temps_Dijkstra";
+        }
+        System.out.println("Le plus rapide : " + fast);
     }
 }
