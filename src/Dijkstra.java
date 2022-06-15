c064cd3588ed23d8acc4c5aa28b5d0f538f4b49b
victorL <victor.lath9@etu.univ-lorraine.fr>
2022-06-15 15:32:40 +0200

    ajout Question21

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -40,6 +40,47 @@ public class Dijkstra implements Algorithme {
         return v;
     }
 
+    /**
+     * Methode permettant d'appliquer l'algorithme Dijkstra avec un nombre d'iteration
+     * @param g
+     * @param depart
+     * @param nbIteration nombre d'iteration de l'algorithme
+     * @return Valeur
+     */
+    public Valeur resoudre(Graphe g, String depart,int nbIteration) {
+        Valeur v = new Valeur();
+        ArrayList<String> Q = new ArrayList<String>();
+        //initialisation
+        for (int i = 0; i < g.listeNoeuds().size(); i++) {
+            v.setValeur(g.listeNoeuds().get(i), Double.MAX_VALUE);
+            v.setParent(g.listeNoeuds().get(i), null);
+            Q.add(g.listeNoeuds().get(i));
+        }
+        v.setValeur(depart, 0);
+        //début
+        //tant que ensemble non vide
+        int cpt = 0;
+        while (!Q.isEmpty() && cpt < nbIteration) {
+            String sommet_u = rechercheMin(Q,v);
+            Q.remove(sommet_u);
+            //Pour chaque sommet_v de Q tel que l'arc(u,v) existe
+            for(int i = 0; i < g.suivants(sommet_u).size();i++){
+                String sommet_v = g.suivants(sommet_u).get(i).getDest();
+                if(Q.contains(sommet_v)){
+                    double poids_arc_uv = g.suivants(sommet_u).get(i).getCout();
+                    Double D = v.getValeur(sommet_u) + poids_arc_uv;
+                    if(D < v.getValeur(sommet_v)){
+                        v.setValeur(sommet_v,D);
+                        v.setParent(sommet_v,sommet_u);
+                    }
+                }
+            }
+           cpt++;
+        }
+
+        return v;
+    }
+
 
     /**
      *
--- src/Question21.java
+++ src/Question21.java
@@ -1,20 +1,22 @@
 public class Question21 {
     public static void main(String[] args) {
-    //Affiche le contenu de l’objet valeur après chaque itération de chacun des algorithmes.
+
         GrapheListe gl = new GrapheListe("graphes/graphe_boucle.txt");
+        int nbIteration = gl.listeNoeuds().size();
 
         Dijkstra dijkstra = new Dijkstra();
+
         System.out.println("Dijkstra: ");
-        for (int i = 0; i < gl.listeNoeuds().size() ;i++) {
-            Valeur dijkstra_iteration = dijkstra.resoudre(gl, "A");
+        for (int i = 0; i < nbIteration; i++) {
+            Valeur dijkstra_iteration = dijkstra.resoudre(gl, "A", i);
             System.out.println("Iteration " + i + " : ");
             System.out.println(dijkstra_iteration);
         }
 
         BellmanFord bellmanFord = new BellmanFord();
         System.out.println("Bellman Ford:");
-        for (int i = 0; i < gl.listeNoeuds().size(); i++) {
-            Valeur bellmanFord_iteration = bellmanFord.resoudre(gl, "A");
+        for (int i = 0; i < nbIteration; i++) {
+            Valeur bellmanFord_iteration = bellmanFord.resoudre(gl,"A",i);
             System.out.println("Iteration " + i + " : ");
             System.out.println(bellmanFord_iteration);
         }
