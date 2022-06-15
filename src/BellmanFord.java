36c5833d3e599c8068b739c548507864bb6218ea
victorL <victor.lath9@etu.univ-lorraine.fr>
2022-06-15 21:48:24 +0200

    ajout commentaire

--- src/BellmanFord.java
+++ src/BellmanFord.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class BellmanFord implements Algorithme{
 
     /**
-     *
+     * Méthode appliquant l'algorithme de BellmanFord sur un graphe à partir d'un point de départ
      * @param g graphe
      * @param depart point de départ
      * @return retourne un objet Valeur contenant les valeurs de chaque point selon l'algorithme de BellmanFord
@@ -21,14 +21,15 @@ public class BellmanFord implements Algorithme{
         //début
         List<String> sommets = g.listeNoeuds();
         String vtmp = null;
-        //tant que non point fixe
         int cpt = 0;
+        //tant que non egal(v, vtmp) et cpt<nbSommets faire
         while (!v.toString().equals(vtmp) && cpt < sommets.size()) {
+            //   vtmp <- v.toString()
             vtmp = v.toString();
-            //pour chaque sommet
+            //pour chaque sommet v de G faire
             for (int i = 0; i < sommets.size(); i++) {
                 String sommet_u = g.listeNoeuds().get(i);
-                //pour chaque arc du sommet_u
+                //pour chaque arc (u, v) de G faire
                 for (int k = 0; k < g.suivants(sommet_u).size(); k++) {
                     //sommet_u est un antécédent de sommet_v
                     String sommet_v = g.suivants(sommet_u).get(k).getDest(); //on a récupéré l'arc(u,v)
@@ -36,13 +37,13 @@ public class BellmanFord implements Algorithme{
 
                     //on prend la valeur d'un des arcs partant de u
                     double cout_arc_uv = g.suivants(sommet_u).get(k).getCout();
-                    //on prend la valeur de sommet_u(antécédent de sommet_v) + le coût de l'arc(u,v)
+                    //tmp <- u.distance + poids(u, v)
                     double tmp = v.getValeur(sommet_u) + cout_arc_uv;
-                    //si la nouvelle distance est meilleur que la valeur du sommet_v
+                    // si tmp < v.distance alors
                     if (tmp < v.getValeur(sommet_v)) {
-                        //on met la nouvelle valeur du sommet_v
+                        // v.distance <- tmp
                         v.setValeur(sommet_v, tmp);
-                        //on met en parent le sommet_u
+                        // v.précédent <- u
                         v.setParent(sommet_v, sommet_u);
                     }
                 }
