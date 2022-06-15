593b3d6ffa02f83a66620ebdddff14e894289339
victorL <victor.lath9@etu.univ-lorraine.fr>
2022-06-15 20:03:51 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Question 15
+        GrapheListe gl = new GrapheListe("graphes/graphe_exemple1.txt");
 
-        GrapheListe gl = new GrapheListe();
-        gl.ajouterArc("A", "B", 12);
-        gl.ajouterArc("A", "D", 87);
-        gl.ajouterArc("B", "E", 11);
-        gl.ajouterArc("C", "A", 19);
-        gl.ajouterArc("D", "C", 10);
-        gl.ajouterArc("D", "B", 23);
-        gl.ajouterArc("E", "D", 43);
-
-        System.out.println(gl);
-
+        //test de la méthode resoudre de BellmanFord
         BellmanFord bellmanFord = new BellmanFord();
         Valeur v = bellmanFord.resoudre(gl, "A");
         System.out.println(v.toString());
